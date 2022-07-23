976e1c06225c8435fd7974e6f8658e1c07e44e1d
lsjpjs1 <lsjpjs1@naver.com>
2022-07-23 16:08:27 +0900

    first commit

---
+++ src/main/java/com/example/hotdealnoti/HotDealNotificationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotdealnoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDealNotificationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotDealNotificationSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hotdealnoti/HotDealNotificationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotdealnoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotDealNotificationSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
