b26bf4c1b189c3545dc93369942339533da3eee5
lsjpjs1 <lsjpjs1@naver.com>
2022-08-03 00:01:42 +0900

    fix: 크롤링 스케줄러 4시간 간격 최종 완성

--- src/main/java/com/example/hotdealnoti/hotdeal/scheduler/HotDealScrapingScheduler.java
+++ src/main/java/com/example/hotdealnoti/hotdeal/scheduler/HotDealScrapingScheduler.java
@@ -15,15 +15,13 @@ import java.io.InputStreamReader;
 @RequiredArgsConstructor
 public class HotDealScrapingScheduler {
 
-    @Scheduled(cron = "0 0/1 * * * ?")
+    @Scheduled(cron = "0 0 0/4 * * ?")
     public void scrapingSchedule() throws IOException {
         log.info("success");
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command("sh", "/home/ubuntu/startScraping.sh","&");
-//        processBuilder.command("su","-s","/bin/sh","ubuntu","-c","export");
         processBuilder.directory(new File("/home/ubuntu"));
         Process p = processBuilder.start();
-//        java.lang.Process p = Runtime.getRuntime().exec("cd /home/ubuntu;export");
         BufferedReader std = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         String outputLine = "";
         while ((outputLine = std.readLine()) != null) {
@@ -35,6 +33,5 @@ public class HotDealScrapingScheduler {
            log.info(outputLine);
         }
 
-        processBuilder.environment().forEach((k,v)->System.out.println(k+" : "+v));
     }
 }
