b1c9f1dd883d125fa4a66e0ba41e48ed44dadcf7
lsjpjs1 <lsjpjs1@naver.com>
2022-08-02 23:19:54 +0900

    fix: 크롤링 스케줄러 실행 쉘 스크립트 실행하게 수정

--- src/main/java/com/example/hotdealnoti/hotdeal/scheduler/HotDealScrapingScheduler.java
+++ src/main/java/com/example/hotdealnoti/hotdeal/scheduler/HotDealScrapingScheduler.java
@@ -18,11 +18,12 @@ public class HotDealScrapingScheduler {
     @Scheduled(cron = "0 0/1 * * * ?")
     public void scrapingSchedule() throws IOException {
         log.info("success");
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-////        processBuilder.command("sh", "/home/ubuntu/startScraping.sh","&");
-//        processBuilder.command("export");
-//        processBuilder.directory(new File(System.getProperty("user.home")));
-        java.lang.Process p = Runtime.getRuntime().exec("cd /home/ubuntu;export");
+        ProcessBuilder processBuilder = new ProcessBuilder();
+//        processBuilder.command("sh", "/home/ubuntu/startScraping.sh","&");
+        processBuilder.command("export");
+        processBuilder.directory(new File("/home/ubuntu"));
+        Process p = processBuilder.start();
+//        java.lang.Process p = Runtime.getRuntime().exec("cd /home/ubuntu;export");
         BufferedReader std = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         String outputLine = "";
         while ((outputLine = std.readLine()) != null) {
