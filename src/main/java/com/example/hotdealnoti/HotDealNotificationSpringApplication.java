fe2d635d37d502cfc03676cc42875d4df9f7f472
lsjpjs1 <lsjpjs1@naver.com>
2022-08-02 20:28:34 +0900

    feat: 크롤링 스케줄러 추가

--- src/main/java/com/example/hotdealnoti/HotDealNotificationSpringApplication.java
+++ src/main/java/com/example/hotdealnoti/HotDealNotificationSpringApplication.java
@@ -3,9 +3,11 @@ package com.example.hotdealnoti;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableScheduling
 public class HotDealNotificationSpringApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/hotdealnoti/hotdeal/scheduler/HotDealScrapingScheduler.java
@@ -0,0 +1,25 @@
+package com.example.hotdealnoti.hotdeal.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class HotDealScrapingScheduler {
+
+    @Scheduled(cron = "59 29 * * * ?")
+    public void scrapingSchedule() throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("nohup","python3","-u","-m","python.scraper.StartScraping","&");
+        processBuilder.directory(new File(System.getProperty("user.home")));
+        processBuilder.start();
+        log.info("success");
+
+    }
+}
